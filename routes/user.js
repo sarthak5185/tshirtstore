6cff7571770cabcee4d33a379adac963c38ceb2f
Sarthak Jain <jainsar123@gmail.com>
2023-01-26 13:16:43 +0530

    reset password functionality added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router()
-const{signup,login,logout,forgotPassword,resetPassword,getLoggedInUserDetails}=require('../controllers/userController');
+const{signup,login,logout,forgotPassword,resetPassword,getLoggedInUserDetails,ChangePassword}=require('../controllers/userController');
 const {isLoggedIn}=require('../middlewares/user');
 router.route("/signup").post(signup);
 router.route("/login").post(login);
@@ -9,5 +9,6 @@ router.route("/forgotPassword").post(forgotPassword);
 router.route("/password/reset/:token").post(resetPassword);
 // isLoggedIn middleware is being injected
 router.route("/userdashboard").get(isLoggedIn,getLoggedInUserDetails);
+router.route("/password/update").put(isLoggedIn,ChangePassword);
 
 module.exports=router;
