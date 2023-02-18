3e6e9f385441710b2616b947b0428f99ef5b126c
Sarthak Jain <jainsar123@gmail.com>
2023-02-18 13:54:21 +0530

    payment method failure method2

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -24,22 +24,31 @@ exports.sendStripeKey = BigPromise(async (req, res, next) => {
 //     //you can optionally send id as well
 //   });
 // });
-// exports.captureStripePayment =(async (req, res) => {
-//   stripe.charges.create(
-//     {
-//       source: req.body.tokenId,
-//       amount: req.body.amount,
-//       currency: "usd",
-//     },
-//     (stripeErr, stripeRes) => {
-//       if (stripeErr) {
-//         res.status(500).json(stripeErr);
-//       } else {
-//         res.status(200).json(stripeRes);
-//       }
-//     }
-//   );
-// });
+exports.captureStripePayment =BigPromise(async (req, res,next) => 
+{
+  console.log(req.body.arr);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    line_items: req.body.arr.map(item => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name:item.name,
+            id:item._id,
+            quantity:item.quantity,
+          },
+          unit_amount:item.price,
+        },
+        quantity: item.quantity,
+      }
+    }),
+    success_url: `${process.env.CLIENT_URL}/Success`,
+    cancel_url: `${process.env.CLIENT_URL}/Cart`,
+  })
+  res.json({ url: session.url })
+})
 // exports.captureStripePayment = BigPromise(async (req, res, next) => {
 //     const {product,token}=req.body;
 //     console.log("Product",product);
@@ -66,22 +75,24 @@ exports.sendStripeKey = BigPromise(async (req, res, next) => {
 //     .then(result=>res.status(200).json(result))
 //     .catch(err=>console.log(err));
 // })
-exports.captureStripePayment = BigPromise(async (req, res, next) => {
-  console.log(`AMOUNT IS :${req.body.amount}`);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: "inr",
-    payment_method: req.body.id,
-    confirm:true,
-     //optional
-     metadata: { integration_check: "accept_a_payment" },
-  });
-  console.log("stripe-routes.js 19 | payment", paymentIntent);
-  res.status(200).json({
-    success: true,
-    amount: req.body.amount,
-    client_secret: paymentIntent.client_secret,
-    //you can optionally send id as well
-  });
-});
+
+
+// exports.captureStripePayment = BigPromise(async (req, res, next) => {
+//   console.log(`AMOUNT IS :${req.body.amount}`);
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: req.body.amount,
+//     currency: "inr",
+//     payment_method: req.body.id,
+//     confirm:true,
+//      //optional
+//      metadata: { integration_check: "accept_a_payment" },
+//   });
+//   console.log("stripe-routes.js 19 | payment", paymentIntent);
+//   res.status(200).json({
+//     success: true,
+//     amount: req.body.amount,
+//     client_secret: paymentIntent.client_secret,
+//     //you can optionally send id as well
+//   });
+// });
 
